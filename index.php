0de112fb83f8b2c120050fadb499269faf03a406
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:46:50 +0200

    boucles

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>exo6</title>
+  </head>
+  <body>
+    <p>
+    <?php
+
+for ($i = 20; $i >0 ; $i--) {
+   echo 'cest presque bon. ';
+}
+?>
+</p>
+  </body>
+</html>
